e34b549805a6b15d4aa90dd1bc7c8e1202b16779
Muigai <muigaiunaka@gmail.com>
2016-10-20 14:25:37 -0400

    updates to transition

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ $(document).ready(function(){
         $('.links').toggleClass('open');
     });
 });
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.add('hide');
+        x[i].classList.add('fade');
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {
+        slideIndex = 1;
+    }
+    x[slideIndex-1].classList.remove('hide');
+    /*x[slideIndex-1].style.display = "flex";*/
+    setTimeout(carousel, 2000); // Change image every 4 seconds
+}
